444b68cca3dd6aab0a6a9b67e7aab017dd9ceaf1
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2022-07-04 04:07:27 +0530

    logging rate of requests

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ let http = require('http').createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(http)
 
+let slotCounter = 0
+let len = 10
+let requestTimeStamp = []
 
 io.on('connection', function(socket){
     socket.on("sendingRequest", function(){
         socket.broadcast.emit("newRequestWasSent")
-        console.log("Heyy I recieved a helloo")
+        requestTimeStamp[slotCounter] = Date.now()
+        let freq = 10*1000/(requestTimeStamp[slotCounter]-requestTimeStamp[(slotCounter+1)%len])
+        console.log(freq)
+        slotCounter++
+        slotCounter = slotCounter%len
+
     })
 })
 app.use(express.static('public'))
