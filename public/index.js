66fa9809a10921ff07e3711997f58e06e654b3de
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2022-07-09 07:31:58 +0530

    Many additions

--- public/index.js
+++ public/index.js
@@ -1,22 +1,99 @@
-let requestTemplate = document.getElementById("requestTemplate").content
-let container = document.querySelector('#container')
+const requestTemplate = document.getElementById("requestTemplate").content
+const failedResponseTemplate = document.getElementById("failedResponse").content
+const succesfulResponseTemplate = document.getElementById("succesfulResponse").content
+const container = document.getElementById('visualisationContainer')
+const requestFrequencySpan = document.getElementById('requestFrequencySpan');
+const responseFrequencySpan = document.getElementById('responseFrequencySpan');
+const throttlingRatioSpan = document.getElementById('throttlingRatioSpan');
+const requestStatus = document.getElementById('responseStatus')
 const socket = io('/')
+let myColor
+
+socket.on('color', (color)=>{
+    myColor = color
+})
+
+socket.on("newRequestWasSent", function(responseMetadata){
+    addRequest(responseMetadata['color'])
+    refreshMetrics(responseMetadata)
+    setTimeout(()=>{
+        addResponse(responseMetadata['isDenied'])
+        refreshChart(responseMetadata)
+
+    },650)
+})
+
+socket.on("requestStatus", function(responseMetadata){
+    refreshMetrics(responseMetadata)
+    requestStatus.innerHTML = responseMetadata.isDenied? "Denied" : "Served"
+    requestStatus.style.color = responseMetadata.isDenied? 'red' : 'green'
+    setTimeout(()=>{
+        addResponse(responseMetadata['isDenied'])
+        refreshChart(responseMetadata)
+    },650)
+})
 
 function sendRequest(){
-    addRequest()
+    addRequest(myColor)
     socket.emit("sendingRequest")
 }
 
-socket.on("newRequestWasSent", function(){
-    addRequest()
-})
-
-function addRequest(){
+function addRequest(color){
     let requestFragment = requestTemplate.cloneNode(true)
     let newAddedRequest = container.appendChild(requestFragment)
-    console.log(newAddedRequest)
+    requestTemplate.children[0].style.backgroundColor = color
     setTimeout(()=>{
         container.removeChild(document.querySelectorAll('.request')[0])
-    
     }, 1500)
+}
+function addResponse(isDenied){
+    let responseFragment = isDenied?failedResponseTemplate.cloneNode(true): succesfulResponseTemplate.cloneNode(true)
+    let newAddedResponse = container.appendChild(responseFragment)
+    setTimeout(()=>{
+        container.removeChild(document.querySelectorAll('.response')[0])
+    }, 1500)
+}
+
+function refreshMetrics(responseMetadata){
+    requestFrequencySpan.innerHTML = responseMetadata.requestFrequency
+    responseFrequencySpan.innerHTML = responseMetadata.servedRequestFrequency
+    throttlingRatioSpan.innerHTML = responseMetadata.throttlingRatio
+    console.log(responseMetadata)
+}
+
+//chart
+const chartContainer = document.getElementById('chartContainer')
+google.charts.load('current', {packages: ['corechart']}).then(initialiseChart)
+let data, chart, initialTime, options
+
+function initialiseChart(){
+    initialTime = Date.now()
+    data = new google.visualization.DataTable()
+    data.addColumn('number', 'Time Elapsed');
+    data.addColumn('number', 'Average response time')
+    data.addColumn('number', 'Average response time without throttling')
+    data.addColumn('number', 'Permitted average response time')
+    options = {
+        height:450,
+        curveType:'function',
+        series: {2:{lineDashStyle:[5,5], color : '#260e0c'}},
+        vAxis:{
+            viewWindow: {
+                min:0,
+                max: 15
+            }
+        },
+        chartArea: {width: '70%', height: '80%'}
+    }
+    chart = new google.visualization.LineChart(chartContainer)
+    chart.draw(data, options);
+}
+let count = 0
+function refreshChart(responseMetadata){
+    let currentTime = (Date.now()-initialTime)/1000
+    data.addRow([currentTime, responseMetadata.servedRequestFrequency, responseMetadata.requestFrequency, 4])
+    while(currentTime-data.getValue(0,0)>20){
+        data.removeRow(0)
+    }
+    chart.draw(data, options)
 }
--- server.js
+++ server.js
@@ -1,22 +1,97 @@
-let express = require('express')
-let app = express()
-let http = require('http').createServer(app)
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(http)
+const {v4:uuidv4} = require('uuid')
+const randomColor = require('randomcolor')
+
+let colors = {}
 
 let slotCounter = 0
-let len = 30
+let requestCounter = 0
+let servedRequestCounter = 0
+let len = 10
+let servedRequestTimeStamp = []
 let requestTimeStamp = []
+let permittedResponseTime = 4
+let windowSeconds = 5
+
+function checkOverloaded(responseTime){
+
+    let percentageOverload = 100*responseTime/permittedResponseTime
+    let mod = 1
+    if (percentageOverload < 100)
+    {
+        mod = 1;               
+    }
+    else if (percentageOverload < 120)
+    {
+        mod = 2;
+    }
+    else if (percentageOverload < 140)
+    {
+        mod = 3;
+    }
+    else if (percentageOverload < 160)
+    {
+        mod = 4;
+    }
+    else if(percentageOverload >= 160)
+    {
+        mod = 10;
+    }
+    // console.log(responseTime)
+    if(requestCounter%mod==0){
+        return {throttlingRatio:mod-1, isDenied:false}
+    }
+    else{
+        return {throttlingRatio:mod-1, isDenied:true}
+    }
+    
+}
 
 io.on('connection', function(socket){
+    colors[socket.id] = randomColor({luminosity:'light'})
+    io.to(socket.id).emit('color', colors[socket.id])
     socket.on("sendingRequest", function(){
-        socket.broadcast.emit("newRequestWasSent")
-        requestTimeStamp[slotCounter] = Date.now()
-        let freq = len*1000/(requestTimeStamp[slotCounter]-requestTimeStamp[(slotCounter+1)%len])
-        console.log(freq)
-        slotCounter++
-        slotCounter = slotCounter%len
-
+        // let servedRequestFrequency = len*1000/(Date.now()-servedRequestTimeStamp[slotCounter])
+        var windowIndex = servedRequestTimeStamp.findIndex((time)=>Date.now()-time<1000*windowSeconds)
+        let servedRequestFrequency
+        if(windowIndex!=-1){
+            servedRequestFrequency = (servedRequestTimeStamp.length - windowIndex)/windowSeconds
+        }
+        else{
+            servedRequestTimeStamp = []
+            servedRequestFrequency = 0
+        }
+        let responseTime = servedRequestFrequency
+        let {throttlingRatio, isDenied} = checkOverloaded(responseTime)
+        if(isDenied==undefined || !isDenied){
+            servedRequestTimeStamp[servedRequestCounter] = Date.now()
+            servedRequestCounter++
+        }
+        var windowIndex = requestTimeStamp.findIndex((time)=>Date.now()-time<1000*windowSeconds)
+        let requestFrequency
+        if(windowIndex!=-1){
+            requestFrequency = (requestTimeStamp.length - windowIndex)/windowSeconds
+        }
+        else{
+            requestTimeStamp = []
+            requestFrequency = 0
+        }
+        
+        console.log(requestFrequency)
+        // for(let i = 0; i<index; i++)
+        //     requestTimeStamp.shift()
+        //
+        // let requestFrequency = len*1000/(Date.now()-requestTimeStamp[requestCounter%len])
+        requestTimeStamp[requestCounter] = Date.now()
+        requestCounter++
+        requestMetrics = {isDenied:isDenied, throttlingRatio:throttlingRatio, requestFrequency:requestFrequency, servedRequestFrequency:servedRequestFrequency, color:colors[socket.id]}
+        // console.log(requestMetrics.isDenied)
+        socket.broadcast.emit("newRequestWasSent", requestMetrics)
+        io.to(socket.id).emit("requestStatus", requestMetrics)
     })
 })
 app.use(express.static('public'))
